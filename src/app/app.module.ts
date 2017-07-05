13b0e614414105781c4879fa5bf72bb48f35ddad
shobhad09 <shobha@tenettech.com>
2017-07-05 12:52:38 +0530

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {rootcomponent} from  './rootcomponent';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    rootcomponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [rootcomponent]
 })
 export class AppModule { }
---
+++ src/app/rootcomponent.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector:'root-component',
+    templateUrl:'./rootcomponent.html',
+    
+
+})
+ export class rootcomponent{
+     title="MY FIRST OWN COMPONENT.";
+ }
